181d02703d2cb3d3cab4fd561eafcd97a7b67c27
JumpingYang001 <v-zhenpy@microsoft.com>
2017-09-13 01:47:47 -0700

    Add Win-CI demo

---
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int actual= Class1.Add(1, 2);
+            int expect = 3;
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
